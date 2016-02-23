b6d2e74c909899c584a7a24a6e151ce37055fe7d
Dan Abramov <dan.abramov@gmail.com>
2016-02-23 16:51:29 +0000

    Add Inspector devtool to real-world example

--- examples/real-world/containers/DevTools.js
+++ examples/real-world/containers/DevTools.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import Inspector from 'redux-devtools-inspector'
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h"
-               changePositionKey="ctrl-w">
-    <LogMonitor />
+               changePositionKey="ctrl-w"
+               changeMonitorKey="ctrl-m">
+    <Inspector />
   </DockMonitor>
 )
